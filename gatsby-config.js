832cd8207cd4e434ba55a36d37ebcc9a03ba4cf2
oisinBates <bates.oisin@gmail.com>
2021-12-23 11:51:26 -0800

    updating package.json and gatsby-config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-minimal-blog/gatsby-config.js
+    pathPrefix: "/oisinbates.github.io",
     siteTitle: `Oisín Bates`,
     siteTitleAlt: `Oisín Bates`,
     siteHeadline: `Personal Blog of Oisín Bates`,
